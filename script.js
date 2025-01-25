4946c61b5d1d66dfe5d0e938114396498bf94abb
humayu <107493734+humayucode@users.noreply.github.com>
2025-01-25 21:09:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function myMenuFunction(){
   
   /* ----- TYPING EFFECT ----- */
   let typingEffect = new Typed(".typedText",{
-    strings : ["Designer","Youtuber","Developer"],
+    strings : ["Designer","Humayu Pathan","Developer"],
     loop : true,
     typeSpeed : 100,
     backSpeed : 80,
